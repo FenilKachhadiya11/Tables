8b35bc27acbe0e7a6a05756cff6d8304374b4d79
Fenil Kachhadiya <kachhadiyafenil11@gmail.com>
2024-06-30 11:33:03 +0530

    Tables

---
+++ Tablesusingforloopandwhileloop.py
@@ -0,0 +1,19 @@
+
+# Using For Loop
+
+num=(int(input("enter the number: ")))
+
+for i in range(11,0,-1):
+    print(f"{num}x{i}={num*i}")
+    
+    
+    
+# Using While Loop 
+    
+    
+num=(int(input("enter the number: ")))
+
+i=1
+while (i<=10):
+    print(f"{num}x{i}={num*i}")
+    i=i+1
